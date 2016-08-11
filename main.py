c9e37a9cc0d6039b220c8f226de00934da5989e1
Nakyoung Kim <nykim@ymail.com>
2016-08-11 22:28:16 +0900

    일단 식수체크못까짐 만듦

--- main.py
+++ main.py
@@ -17,6 +17,8 @@ import urllib2
 import json
 import logging
 import re
+import time
+from datetime import date
 
 # 봇 토큰, 봇 API 주소
 TOKEN = '239557605:AAFPnfm4zOJ6XFK2nByTxdVQZza3WmPbr4E'
@@ -28,6 +30,14 @@ CMD_STOP      = '/stop'
 CMD_HELP      = '/help'
 CMD_BROADCAST = '/broadcast'
 CMD_BROADCAST_SHORTCUT = '/bc'
+CMD_REGISTER_ID = '/register'
+CMD_QUERY_ID = '/query'
+
+# 봇 상태
+STATUS_IDLE = 'IDLE'
+STATUS_DINNER = 'DINNER'
+
+#인라인 커맨드로 긔..긔긔귀요미 사진 추가;
 
 # 봇 사용법 & 메시지
 USAGE = u"""[사용법] 아래 명령어를 메시지로 보내거나 버튼을 누르시면 됩니다.
@@ -42,22 +52,48 @@ MSG_STOP  = u'봇을 정지합니다.'
 CUSTOM_KEYBOARD = [
         [CMD_START],
         [CMD_STOP],
-        [CMD_HELP],
-        ]
+        [CMD_HELP]]
+
+SELECT_DINNER_EAT=u'/ㅇㅇ먹음'
+SELECT_DINNER_NOT=u'/ㄴㄴ안먹음'
+CUSTOM_KEYBOARD_YESNO = [
+        [SELECT_DINNER_EAT],
+        [SELECT_DINNER_NOT]]
+
+class NDB_Account(ndb.Model):
+    id = ndb.StringProperty()
+    name = ndb.StringProperty()
+
+def registerAccount(chat_id, user_id):
+    # 아이디 확인 절차...
+    # - send email to this mail account with a randomly generated passwd
+    # - wait for passwd or quit
+
+    # DB 인스턴스에 저장
+    instance = NDB_Account.get_or_insert('midasAccount')
+    instance.id = user_id
+    instance.put()
+    send_msg(chat_id, u'아이디가 등록됨')
+
+
+def queryAccount(chat_id):
+    account = NDB_Account.get_by_id('midasAccount')
+    send_msg(chat_id, u'등록된 아이디는 ' + account.id)
 
 # 채팅별 봇 활성화 상태
-# 구글 앱 엔진의 Datastore(NDB)에 상태를 저장하고 읽음
+# 구글 앱 엔진의 Datastore(NDB_)에 상태를 저장하고 읽음
 # 사용자가 /start 누르면 활성화
 # 사용자가 /stop  누르면 비활성화
-class EnableStatus(ndb.Model):
+class NDB_SerivceEnableStatus(ndb.Model):
     enabled = ndb.BooleanProperty(required=True, indexed=True, default=False,)
+    status =  ndb.StringProperty(required=True, default=STATUS_IDLE,)
 
 def set_enabled(chat_id, enabled):
     u"""set_enabled: 봇 활성화/비활성화 상태 변경
     chat_id:    (integer) 봇을 활성화/비활성화할 채팅 ID
     enabled:    (boolean) 지정할 활성화/비활성화 상태
     """
-    es = EnableStatus.get_or_insert(str(chat_id))
+    es = NDB_SerivceEnableStatus.get_or_insert(str(chat_id))
     es.enabled = enabled
     es.put()
 
@@ -65,16 +101,16 @@ def get_enabled(chat_id):
     u"""get_enabled: 봇 활성화/비활성화 상태 반환
     return: (boolean)
     """
-    es = EnableStatus.get_by_id(str(chat_id))
+    es = NDB_SerivceEnableStatus.get_by_id(str(chat_id))
     if es:
         return es.enabled
     return False
 
 def get_enabled_chats():
     u"""get_enabled: 봇이 활성화된 채팅 리스트 반환
-    return: (list of EnableStatus)
+    return: (list of NDB_SerivceEnableStatus)
     """
-    query = EnableStatus.query(EnableStatus.enabled == True)
+    query = NDB_SerivceEnableStatus.query(NDB_SerivceEnableStatus.enabled == True)
     return query.fetch()
 
 # 메시지 발송 관련 함수들
@@ -153,6 +189,37 @@ def cmd_echo(chat_id, text, reply_to):
 
 #def cmd_midas_check_dinner(chat_id, text, )
 # http://erp.midasit.com/main_food_ok.asp?c_date=2016-08-04&c_code=nykim&OX=X
+def ask_dinner():
+    d = date.today()
+    msg = u'즐거운 아침입니다. 오늘(' + d.isoformat() + u') 저녁식사 하실건가요? sent from midasit_bot'
+    for chat in get_enabled_chats():
+        send_msg(chat.key.string_id(), msg, keyboard = CUSTOM_KEYBOARD_YESNO)
+        NDB_SerivceEnableStatus.status = STATUS_DINNER
+
+def process_dinner(chat_id, text):
+    # http://erp.midasit.com/main_food_ok.asp?c_date=2016-08-09&c_code=nykim&OX=X
+    data = {}
+    d = date.today()
+    data['c_date'] = d.isoformat()
+    logging.info(NDB_Account.id)
+    account = NDB_Account.get_by_id('midasAccount')
+    logging.info(account)
+    data['c_code'] = account.id
+
+    if SELECT_DINNER_EAT == text:
+        data['OX'] = 'X'
+    elif SELECT_DINNER_NOT == text:
+        data['OX'] = 'O'
+    else:
+        data['OX'] = 'X'
+    url = 'http://erp.midasit.com/main_food_ok.asp?'
+    url_values = urllib.urlencode(data)
+    full_url = url + url_values
+
+    logging.info(full_url)
+    urllib2.urlopen(full_url)
+    NDB_SerivceEnableStatus.status = STATUS_IDLE
+    send_msg(chat_id, u'식수체크 ㄳㄳ', keyboard = CUSTOM_KEYBOARD)
 
 def process_cmds(msg):
     u"""사용자 메시지를 분석해 봇 명령을 처리
@@ -162,7 +229,19 @@ def process_cmds(msg):
     msg_id = msg['message_id']
     chat_id = msg['chat']['id']
     text = msg.get('text')
+
+    logging.info(type(SELECT_DINNER_NOT))
+    logging.info(type(text))
+
+
     if (not text):
+        return
+    if (NDB_SerivceEnableStatus.status == STATUS_DINNER and 
+            (SELECT_DINNER_EAT == text or SELECT_DINNER_NOT == text)):
+        process_dinner(chat_id, text)
+        return
+    if CMD_QUERY_ID == text:
+        queryAccount(chat_id)
         return
     if CMD_START == text:
         cmd_start(chat_id)
@@ -183,6 +262,15 @@ def process_cmds(msg):
     if cmd_broadcast_shortcut_match:
         cmd_broadcast(chat_id, cmd_broadcast_shortcut_match.group(1))
         return
+
+    cmd_register_match = re.match('^' + CMD_REGISTER_ID + ' (.*)', text)
+    cmd_query_match = re.match('^' + CMD_QUERY_ID + ' (.*)', text)
+    if cmd_register_match:
+        registerAccount(chat_id, cmd_register_match.group(1))
+        return
+    if cmd_query_match:
+        queryAccount(chat_id)
+        return 
     cmd_echo(chat_id, text, reply_to=msg_id)
     return
 
@@ -212,16 +300,24 @@ class WebhookHandler(webapp2.RequestHandler):
     def post(self):
         urlfetch.set_default_fetch_deadline(60)
         body = json.loads(self.request.body)
-        print body
+
+        logging.info(self.request)
+        #logging.debug('This is a debug message')
+        #logging.info('This is an info message')
+        #logging.warning('This is a warning message')
+        #logging.error('This is an error message')
+        #logging.critical('This is a critical message')
+        logging.info(body)
+
         self.response.write(json.dumps(body))
-        process_cmds(body['message'])
+        if 'message' in body:
+            process_cmds(body['message'])
 
 class DinnerHandler(webapp2.RequestHandler):
-    def post(self):
+    def get(self):
         urlfetch.set_default_fetch_deadline(60)
-        body = json.loads(self.request.body)
-        self.response.write(json.dumps(body))
-        #process_cmds(body['message'])
+        logging.info(self.request)
+        ask_dinner()
 
 class MainPage(webapp2.RequestHandler):
     def get(self):
